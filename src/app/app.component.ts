f8ed38d89ba3660c8a2b4a90c26fb32deea38bea
wasd <kirshvedov13@gmail.com>
2024-07-31 00:52:18 +0200

    replace login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
-import {LoginComponent} from "./login/login/login.component";
+import {LoginComponent} from "./login/login.component";
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
-import {LoginComponent} from "./login/login/login.component";
+import {LoginComponent} from "./login/login.component";
 
 @NgModule(
   {

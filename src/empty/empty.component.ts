b29a69dab33428abb5d2eb354be93fe93da39a10
wasd <kirshvedov13@gmail.com>
2024-08-01 08:48:05 +0200

    implement empty component

--- src/empty/empty.component.ts
+++ src/empty/empty.component.ts
@@ -1,9 +1,15 @@
 import {Component} from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-empty',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './empty.component.html',
   styleUrl: './empty.component.scss'
 })

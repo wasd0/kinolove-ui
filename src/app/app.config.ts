2b91fee6252f70d208da598e73b464a551b019de
wasd <kirshvedov13@gmail.com>
2024-08-04 11:34:09 +0200

    add auth interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,12 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
-import {provideHttpClient, withFetch} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi} from "@angular/common/http";
+import {AuthInterceptor} from "../interceptors/authInterceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()), {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},]
 };

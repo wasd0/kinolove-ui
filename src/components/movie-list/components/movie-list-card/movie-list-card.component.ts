38031d1e73ab998f7c21133eb9c40c31a6baf9b7
wasd <kirshvedov13@gmail.com>
2024-08-09 19:22:53 +0200

    movie list  fixes

--- src/components/movie-list/components/movie-list-card/movie-list-card.component.ts
+++ src/components/movie-list/components/movie-list-card/movie-list-card.component.ts
@@ -1,13 +1,25 @@
 import {Component, Input} from '@angular/core';
+import {Router} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-movie-list-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './movie-list-card.component.html',
   styleUrl: './movie-list-card.component.scss'
 })
 export class MovieListCardComponent {
   @Input() title: string = "";
   @Input() id: number = 0;
+
+  constructor(private readonly router: Router) {
+  }
+
+  goToMovieCard() {
+    const movieRoute = `movie/${this.id}`
+    this.router.navigate([movieRoute])
+  }
 }

af7c1ef6c5e265025361395bc077bda5179a1768
wasd <kirshvedov13@gmail.com>
2024-08-30 14:50:26 +0200

    add unauthorized error catch

--- src/interceptors/authInterceptor.ts
+++ src/interceptors/authInterceptor.ts
@@ -1,12 +1,24 @@
 import {Injectable} from "@angular/core";
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {catchError, Observable, throwError} from "rxjs";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private readonly router: Router) {
+  }
+
   intercept(req: HttpRequest<any>, handler: HttpHandler): Observable<HttpEvent<any>> {
     const newReq = req.clone({withCredentials: true });
-    return handler.handle(newReq);
+    return handler.handle(newReq).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          this.router.navigate(['/login']);
+        }
+        return throwError(err);
+      })
+    );
   }
 }
 
